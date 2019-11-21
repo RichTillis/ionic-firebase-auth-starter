c720ea849be5417cacfdf19569daccc3382c994a
Rich Tillis <rich@tillis.io>
2019-11-20 19:45:39 -0700

    updated logout UI

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,6 +3,7 @@ import { Router } from "@angular/router";
 
 import { AuthService } from "../../services/auth/auth.service";
 import { ToastService } from "../../services/toast/toast.service";
+import { LoadingService } from "../../services/loading/loading.service";
 
 @Component({
   selector: "app-home",
@@ -13,12 +14,15 @@ export class HomePage implements OnInit {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private toastService: ToastService
+    private toastService: ToastService,
+    private loadingService: LoadingService
   ) {}
 
   ngOnInit() {}
 
   logout() {
+    // this.logoutProcessing();
+
     this.authService
       .logoutUser()
       .then(() => {
@@ -28,7 +32,15 @@ export class HomePage implements OnInit {
       .catch(error => console.log(error));
   }
 
-  logoutSucess(){
+  logoutProcessing() {
+    this.loadingService.present({
+      message: "Logging out. . ."
+    });
+  }
+
+  logoutSucess() {
+    this.loadingService.dismiss();
+
     this.toastService.present({
       message: "Logout successful. See ya next time!",
       duration: 3000,
