e0723fc55fdd901a71974c6cb0d513f1657465d1
Rich Tillis <rich.tillis@learninga-z.com>
2020-01-08 10:13:00 -0700

    comment to update firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+//The Firebase config needs to be replaced with a new one. This firebaseConfig links to a project that has been deleted
 export const environment = {
   production: false,
   
